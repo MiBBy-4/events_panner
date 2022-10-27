628482cebe101daabc6a46142cef0e7597aa6174
MiBBy-4 <maksim.762373maks@bk.ru>
2022-10-27 15:43:03 +0300

    fix: resolve

--- db/migrate/20221026122056_create_first_name_and_last_name_in_users.rb
+++ db/migrate/20221026122056_create_first_name_and_last_name_in_users.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class CreateFirstNameAndLastNameInUsers < ActiveRecord::Migration[7.0]
-  # rubocop:disable Rails/BulkChangeTable
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
-  # rubocop:enable Rails/BulkChangeTable
 end
