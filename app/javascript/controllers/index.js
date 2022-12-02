b6f5d448c2f7acb453cb709591d5f8b19dcdf0b9
MiBBy-4 <maksim.762373maks@bk.ru>
2022-12-02 16:55:14 +0300

    set to deploy

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,6 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "./application";
+
+import EventsController from "./events_controller";
+application.register("events", EventsController)
